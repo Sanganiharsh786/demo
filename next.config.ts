6c4dae4b8ebfee0b2635d1371e6fe58b1480e634
mitulkanani <sanganiharsh786@gmail.com>
2024-10-22 16:06:52 +0530

    Latest  Code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/about/:path*",
+        destination: "/contact/:path*",
+        permanent: true,
+        locale: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
